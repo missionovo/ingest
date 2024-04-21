792718e2b89345c0eae08d2a6566a97555afa75a
sclinebacker84 <aakash.mehta18@gmail.com>
2024-04-21 14:51:29 -0400

    fixed typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]; then
     echo 'specify customer name as first arg to this script'
     exit 1
 fi
-pushd $(dirname 0)
+pushd $(dirname $0)
 curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
 cp fluent-bit.conf /etc/fluent-bit/fluent-bit.conf
 cp auditd.rules /etc/audit/rules.d/auditd.rules
